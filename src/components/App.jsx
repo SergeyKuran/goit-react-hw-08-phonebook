003b6327bc96450b44cd9751f600ddd0df401f6d
Sergey Kuran <kuran.serg@gmail.com>
2023-08-04 07:27:40 +0300

    Update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,68 @@
+import { useAuth } from 'hooks/useAuth';
+import { lazy, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { refreshUser } from 'redux/auth/fetch';
+import { Layout } from './Layout';
+
+const Contacts = lazy(() => {
+  import('./pages/Contacts/Contacts');
+});
+const Home = lazy(() => import('./pages/Home/Home'));
+const Login = lazy(() => import('./pages/Login/Login'));
+const Registration = lazy(() => import('./pages/Registration/Registration'));
+
 export const App = () => {
+  const dispatch = useDispatch();
+  const { isRefreshing } = useAuth();
+  const { isLoginIn } = useAuth();
+
+  useEffect(() => {
+    dispatch(refreshUser());
+  }, [dispatch]);
+
+  const RestrictedRoute = ({ component: Component, redirectTo = '/' }) => {
+    return isLoginIn ? <Navigate to={redirectTo} /> : Component;
+  };
+
+  const PrivateRoute = ({ component: Component, redirectTo = '/' }) => {
+    const shouldRedirect = !isLoginIn && !isRefreshing;
+
+    return shouldRedirect ? <Navigate to={redirectTo} /> : Component;
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      {isRefreshing ? (
+        <b>Loading...</b>
+      ) : (
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route
+              path="/register"
+              element={
+                <RestrictedRoute
+                  redirectTo="/contacts"
+                  component={<Registration />}
+                />
+              }
+            />
+            <Route
+              path="/login"
+              element={
+                <RestrictedRoute redirectTo="/contacts" component={<Login />} />
+              }
+            />
+            <Route
+              path="/contacts"
+              element={
+                <PrivateRoute redirectTo="/contacts" component={<Contacts />} />
+              }
+            />
+          </Route>
+        </Routes>
+      )}
+    </>
   );
 };
--- src/components/ContactList/ContactList.styled.js
+++ src/components/ContactList/ContactList.styled.js
@@ -1,12 +1,16 @@
 import styled from 'styled-components';
 
-export const Ul=styled.ul`text-align: start;`
-export const Li=styled.li`
+export const Ul = styled.ul`
+  text-align: start;
+`;
+
+export const Li = styled.li`
   display: flex;
   align-items: center;
-  justify-content: space-around;`
-export const Button=styled.button`
-  padding: 2px 10px;
-  border-radius: 10px;`
+  justify-content: space-around;
+`;
 
-  
+export const Button = styled.button`
+  padding: 2px 10px;
+  border-radius: 10px;
+`;
