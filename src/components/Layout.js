b63a05e08a8f8e5b63404601a9aedc7ccffa4558
Sergey Kuran <kuran.serg@gmail.com>
2023-08-04 07:27:00 +0300

    Create Layout.js

---
+++ src/components/Layout.js
@@ -0,0 +1,15 @@
+import { Container } from './App.styled';
+import { Outlet } from 'react-router-dom';
+import { Suspense } from 'react';
+import { AppAuth } from 'AppAuth/AppAuth';
+
+export const Layout = () => {
+  return (
+    <Container>
+      <AppAuth />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Outlet />
+      </Suspense>
+    </Container>
+  );
+};
