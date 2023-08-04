58300f66b7c6cd553222a855e194dab6ab52cb91
Sergey Kuran <kuran.serg@gmail.com>
2023-08-04 07:25:01 +0300

    Update

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
 import { ChakraProvider } from '@chakra-ui/react';
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import { store } from 'redux/contacts/store';
-import App from './components/App';
-
-import './index.css';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor, store } from 'redux/contacts/store';
+import { App } from './components/App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <ChakraProvider>
-          <App />
-        </ChakraProvider>
-      </Provider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter basename="/goit-react-hw-08-phonebook">
+          <ChakraProvider>
+            <App />
+          </ChakraProvider>
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
