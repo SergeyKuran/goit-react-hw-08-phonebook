33d42cb028fd7592bcc178b79971206a1b7b0641
Sergey Kuran <kuran.serg@gmail.com>
2023-08-04 07:28:17 +0300

    Create ContactLIst.jsx

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { deleteContact } from 'redux/contacts/fetch';
+import { getFilter } from 'redux/contacts/filterSlice';
+import { getContacts } from 'redux/contacts/selector';
+import { Button, Li, Ul } from './ContactList.styled';
+
+export const ContactList = () => {
+  const dispatch = useDispatch();
+  const contacts = useSelector(getContacts);
+  const selector = useSelector(getFilter);
+
+  const getVisible = () =>
+    contacts.filter(e => e.name.toLowerCase().includes(selector.toLowerCase()));
+  const getVisibleContacts = getVisible();
+
+  return (
+    <div>
+      <Ul>
+        {getVisibleContacts.map(({ id, name, phone }) => (
+          <Li key={id}>
+            {name}: {phone}
+            <Button
+              type="button"
+              onClick={() => {
+                dispatch(deleteContact(id));
+              }}
+            >
+              Delete
+            </Button>
+          </Li>
+        ))}
+      </Ul>
+    </div>
+  );
+};
