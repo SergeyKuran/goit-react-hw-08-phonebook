ad06faf0d282dc1244510595aa954a49fbdf6a7a
Sergey Kuran <kuran.serg@gmail.com>
2023-08-04 07:25:43 +0300

    Update

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,5 +1,23 @@
-const UserMenu = () => {
-  return <></>;
-};
+import { Button } from '@chakra-ui/react';
+import { useAuth } from 'hooks/useAuth';
+import { useDispatch } from 'react-redux';
+import { logOutUser } from 'redux/auth/fetch';
 
-export default UserMenu;
+export const UserMenu = () => {
+  const dispath = useDispatch();
+  const { user } = useAuth();
+
+  return (
+    <div>
+      <p>Hello ${user.name}</p>
+      <Button
+        type="button"
+        onClick={() => {
+          dispath(logOutUser());
+        }}
+      >
+        Logout
+      </Button>
+    </div>
+  );
+};
